f310835d63525449661248e0efbf057c3b50e0d7
shaikhahan03 <shaikhahan03@gmail.com>
2026-04-26 23:38:34 +0530

    Implement game menu for Stone Paper Scissors

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import play_game
+import utils
 
+def game_menu():
+    while True:
+        print("\n--- Stone Paper Scissors ---")
+        print("1. Play Game")
+        print("2. Show Score")
+        print("3. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            play_game.play()
+        elif choice == "2":
+            utils.show_score()
+        elif choice == "3":
+            print("Exiting game...")
+            break
+        else:
+            print("Invalid choice!")
+
+game_menu()
