436d8320bf2f17976993d1067184bbf600540d15
shaikhahan03 <shaikhahan03@gmail.com>
2026-04-26 23:39:06 +0530

    Implement rock-paper-scissors game logic

--- play_gmae.py
+++ play_gmae.py
@@ -1,1 +1,27 @@
+import random
+import result
+import utils
 
+def play():
+    options = ["stone", "paper", "scissors"]
+
+    user_choice = input("Enter stone, paper, or scissors: ").lower()
+
+    if user_choice not in options:
+        print("Invalid choice!")
+        return
+
+    computer_choice = random.choice(options)
+
+    print("Computer chose:", computer_choice)
+
+    winner = result.check_winner(user_choice, computer_choice)
+
+    if winner == "user":
+        print("You win!")
+        utils.user_score += 1
+    elif winner == "computer":
+        print("Computer wins!")
+        utils.computer_score += 1
+    else:
+        print("It's a draw!")
