7c5e9bcb18102cff4cbdc97be1466ff336f84661
shaikhahan03 <shaikhahan03@gmail.com>
2026-04-26 23:39:33 +0530

    Implement check_winner function for game logic
    Add function to check the winner of the game

--- result.py
+++ result.py
@@ -1,1 +1,13 @@
+def check_winner(user, computer):
+    if user == computer:
+        return "draw"
 
+    elif (
+        (user == "stone" and computer == "scissors") or
+        (user == "paper" and computer == "stone") or
+        (user == "scissors" and computer == "paper")
+    ):
+        return "user"
+
+    else:
+        return "computer"
